0a0cc3232644c8fae971b8deadf90cf5a57643de
Anastasia Andreeva <nastiy1890@yandex.ru>
2023-08-28 18:51:52 -0700

    add signout page object

--- tests/pageObject/components/SignOut.ts
+++ tests/pageObject/components/SignOut.ts
@@ -2,6 +2,7 @@ import {Page, Locator} from "@playwright/test";
 export default class SignOut {
   readonly page: Page
   readonly signOutLink: Locator;
+  
   constructor(page: Page) {
     this.page = page;
     this.signOutLink = page.getByText('SignOut');
